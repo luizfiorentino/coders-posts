469139a80d9f5bb09dd528fab1a2785ac12fed61
luizfiorentino <lgfiorentino@gmail.com>
2022-05-16 22:59:41 +0200

    feat/ fetching posts with thunk

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "../store/feed/actions";
+import { selectFeedPosts } from "../store/feed/selectors";
 
 export default function HomePage() {
-  return <h2>Hey there!</h2>;
+  const dispatch = useDispatch();
+  const posts = useSelector(selectFeedPosts);
+
+  useEffect(() => {
+    dispatch(fetchPosts);
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>Posts Feed</h2>
+      <ul>
+        {!posts.length
+          ? "Loading"
+          : posts.map((post) => (
+              <li key={post.id}>
+                {post.title}
+                <p>{post.content}</p>
+              </li>
+            ))}
+      </ul>
+    </div>
+  );
 }
